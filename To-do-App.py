ee1f4d8cb71fb9a5d85f3f3310e0073b68611b1d
gabrieldiaztoro <gdiaz_toro@hotmail.com>
2022-09-28 19:47:05 -0300

    inserindo label (inserir nova tarefa)

--- To-do-App.py
+++ To-do-App.py
@@ -40,11 +40,18 @@ frame_e_baixo = Frame(frame_esquerda, width=300, height=150,
                       bg=cor2, relief="flat")
 frame_e_baixo.grid(row=1, column=0, sticky=NSEW)
 
-#(PASSO 2) criando função para os botoes NOVO E ATUALIZAR
+# (PASSO 2) criando função para os botoes NOVO E ATUALIZAR
+
+
 def main(a):
     # NOVO
     if a == "novo":
         print("novo")
+
+    label = Label(frame_e_baixo, text="Insira nova tarefa", width=42, height=1, pady=7,
+                  padx=10, relief="flat", anchor=W, fg=cor0, bg=cor2)
+    label.grid(row=0, column=0, sticky=NSEW, pady=1)
+
     # ATUALIZAR
     if a == "atualizar":
         print("atualizar")
@@ -52,17 +59,19 @@ def main(a):
 
 # ----CRIANDO BOTOES
 b_novo = Button(frame_e_cima, text="NOVO", width=10, height=1,  # frame_e_cima# foi colocado ao vez da freme principal
-                relief=RAISED, command=lambda:main("novo"), bg=cor3, fg="white", anchor="center", overrelief='ridge', font=("Arial 10 bold"))  # foi usado grid para criar botao ao vez de place
-b_novo.grid(row=0, column=0, sticky=NSEW, pady=1) #adicionado  command=lambda:main("novo"), depois do PASSO 2
+                relief=RAISED, command=lambda: main("novo"), bg=cor3, fg="white", anchor="center", overrelief='ridge', font=("Arial 10 bold"))  # foi usado grid para criar botao ao vez de place
+# adicionado  command=lambda:main("novo"), depois do PASSO 2
+b_novo.grid(row=0, column=0, sticky=NSEW, pady=1)
 
 
 b_remover = Button(frame_e_cima, text="REMOVER", width=10, height=1,  # frame_e_cima# foi colocado ao vez da freme principal
-                   relief=RAISED, command=lambda:main("atualizar"), bg=cor4, fg="white", anchor="center", overrelief='ridge', font=('Arial 10 bold'))  # foi usado grid para criar botao ao vez de place
-b_remover.grid(row=0, column=1, sticky=NSEW, pady=1) #adicionado  command=lambda:main("atualizar"), depois do PASSO 2
+                   relief=RAISED, bg=cor4, fg="white", anchor="center", overrelief='ridge', font=('Arial 10 bold'))  # foi usado grid para criar botao ao vez de place
+b_remover.grid(row=0, column=1, sticky=NSEW, pady=1)
 
 
 b_atualizar = Button(frame_e_cima, text="ATUALIZAR", width=10, height=1,  # frame_e_cima# foi colocado ao vez da freme principal
-                     relief=RAISED, bg=cor5, fg="white", anchor="center", overrelief='ridge', font=("Arial 10 bold"))  # foi usado grid para criar botao ao vez de place
+                     relief=RAISED, command=lambda: main("atualizar"), bg=cor5, fg="white", anchor="center", overrelief='ridge', font=("Arial 10 bold"))  # foi usado grid para criar botao ao vez de place
+# adicionado  command=lambda:main("atualizar"), depois do PASSO 2
 b_atualizar.grid(row=0, column=2, sticky=NSEW, pady=1)
 
 
